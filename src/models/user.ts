0dbf05af507a5947abe267555c2499e1714dc8ff
EduGese <122921699+EduGese@users.noreply.github.com>
2024-02-02 18:27:07 +0100

    Change atribute role in UserShema:  - role regular as default

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,26 +2,28 @@ import mongoose, { Schema } from "mongoose";
 import { User } from "../interfaces/user.interface";
 
 const UserSchema: Schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "regular",
+  },
+  favorites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Favorites",
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    favorites: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Favorites'
-    }],
+  ],
 });
 
-export default mongoose.model<User>('User', UserSchema);
+export default mongoose.model<User>("User", UserSchema);
