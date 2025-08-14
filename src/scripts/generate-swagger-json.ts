c9195e8a2dccda9e73b0c59aa672888367fc26a1
EduGese <122921699+EduGese@users.noreply.github.com>
2025-08-14 12:45:47 +0200

    fix: update swagger.json generation and content
    - Updated email contact in swagger.json
    - Removed unused security scheme and added Bearer token authentication
    - Enhanced error handling for unauthorized access
    - Improved swagger.json generation script for local and production environments

--- src/scripts/generate-swagger-json.ts
+++ src/scripts/generate-swagger-json.ts
@@ -1,8 +1,12 @@
 import fs from 'fs';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { options } from '../utils/swagger/swagger';
+import path from 'path';
 
 const swaggerSpec = swaggerJsdoc(options);
 
-fs.writeFileSync('swagger.json', JSON.stringify(swaggerSpec, null, 2), 'utf-8');
-console.log('swagger.json generated!');
+fs.writeFileSync(path.resolve('swagger.json'), JSON.stringify(swaggerSpec, null, 2), 'utf-8');
+console.log('Local swagger.json generated!');
+
+fs.writeFileSync(path.resolve('docs/swagger.json'), JSON.stringify(swaggerSpec, null, 2), 'utf-8');
+console.log('Prod swagger.json generated!');
