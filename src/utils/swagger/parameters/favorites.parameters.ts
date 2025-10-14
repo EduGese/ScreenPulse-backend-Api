f9eef02397af3d2d2fddc30483eb599a3010b9a5
EduGese <122921699+EduGese@users.noreply.github.com>
2025-10-14 20:07:50 +0200

    refactor: remove unused userId parameter from favorites
    - Deleted commented-out UserIdParam from favorites parameters

--- src/utils/swagger/parameters/favorites.parameters.ts
+++ src/utils/swagger/parameters/favorites.parameters.ts
@@ -1,12 +1,4 @@
 export const favoritesParameters = {
-  // UserIdParam: {
-  //   name: 'userId',
-  //   in: 'path',
-  //   required: true,
-  //   example: '60c72b2f9b1e8b001c8e4d3a',
-  //   schema: { type: 'string' },
-  //   description: 'ID of the user to retrieve favorites for.',
-  // },
   MediaItemIdParams: {
     name: 'mediaItemId',
     in: 'path',
